9cf6b5a22d2add6e322a295225ebbc9803dce6b3
rafaelmsales27 <rafaelmsales27@gmail.com>
2025-05-19 22:26:37 +0200

    add alert, number of days

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -74,3 +74,30 @@
     </div>
 
 </body>
+
+<script>
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+function success(pos) {
+  const crd = pos.coords;
+
+  console.log("Your current position is:");
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+
+  document.getElementById('latitude').value = crd.latitude;
+    document.getElementById('longitude').value = crd.longitude;
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+</script>
